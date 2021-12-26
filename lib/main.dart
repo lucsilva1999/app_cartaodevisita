2b59fe405a9eeeb971ea29d09d44dab680f7030f
lucsiva1999 <lucsilva1999@gmail.com>
2021-12-26 04:29:47 -0300

    Trocando container pelo column

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,15 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.deepOrange,
         body: SafeArea(
-          child: Container(
-            padding: EdgeInsets.all(20.0),
-            margin: EdgeInsets.only(left: 30.0),
-            height: 100.0,
-            width: 100.0,
-            color:  Colors.white,
-            child: Text("Olá pessoal")
+          child: Column(
+            children: [
+              Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color:  Colors.white,
+                  child: Text("Container 1")
+              )
+            ],
           )
         ),
       ),
