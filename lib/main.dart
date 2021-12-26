47d8aee4f3eb7275ac7983a45d352a73c059e66a
lucsiva1999 <lucsilva1999@gmail.com>
2021-12-26 04:22:56 -0300

    Teste de commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.deepOrange,
         body: SafeArea(
           child: Container(
-            padding: EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(30.0),
             margin: EdgeInsets.only(left: 30.0),
             height: 100.0,
             width: 100.0,
